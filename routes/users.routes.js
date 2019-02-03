6af96080b8efd8c4ab0ad39f3d160e62c7fabd56
Clara Garcia <clara.garcia.pascual@accenture.com>
2019-02-03 21:22:21 +0100

    menu header done

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usersController = require ('../controllers/users.controller.js');
 const secure = require('../middlewares/auth.middleware');
 
-router.get('/profile', usersController.profile);
+router.get('/profile/index', usersController.profile);
 router.get('/profile/edit', usersController.edit);
 router.post('/profile/edit', usersController.doEdit);
 router.get('/matcheaks', secure.isAuthenticated, secure.isProfileCompleted, usersController.list);
