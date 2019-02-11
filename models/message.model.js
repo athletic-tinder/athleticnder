6047343a95d69f1fecd3dd100dbbd54d29a3ae1f
Nerea <nereaparicio@gmail.com>
2019-02-11 21:48:23 +0100

    mis matches list

--- models/message.model.js
+++ models/message.model.js
@@ -3,16 +3,23 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   from: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
   },
   to: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
   },
   text:{
     type: String,
-    max: 2000
+    maxlength: 2000
   }
 }, { timestamps: true });
 
 const Message = mongoose.model('Message', schema);
 
 module.exports = Message;
+
+/**
+ 
+$or: [{form: req.user.id, to:req.params.id},{form:req.params.id, to:req.user.id}]
+ */
