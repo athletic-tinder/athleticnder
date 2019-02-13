a27f3900269fde57536c03852db936403fa261aa
Clara Garcia <clara.garcia.pascual@accenture.com>
2019-02-13 20:35:44 +0100

    working on messages controller

--- routes/message.routes.js
+++ routes/message.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const messageController = require ('../controllers/message.controller');
 
-router.get('/:userId', messageController.getRoom);
+router.get('messages/:userId', messageController.getRoom);
 // router.post('/messages/:userId', messageController.sendMessage);
 
 module.exports = router;
