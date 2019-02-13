d11d7a1c30500569fc2d41c6dec02fa4e5dc839f
Clara Garcia <clara.garcia.pascual@accenture.com>
2019-02-13 21:09:38 +0100

    working on messages controller

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -2,6 +2,27 @@ const Message = require('../models/message.model');
 
 module.exports.getRoom = (req, res, next ) => {
   Message.find({
-    $or: [{form: req.user.id, to:req.params.id},{form:req.params.id, to:req.user.id}]
+    $or: [{from: req.user.id, to:req.params.id},{from:req.params.id, to:req.user.id}]
   })
-};
+  .populate('from')
+  .populate('to')
+  .then(messages => {
+    console.log({ messages })
+    res.render('messages/messages', { messages, interestedId: req.params.id });
+  })
+  .catch(error => next(error))
+}
+
+module.exports.sendMessage = (req, res, next) => {
+  // Traernos el id del otro
+  // Crear un nuevo mensaje (yo soy el from, el otro usuario el to, y req.body.text el text)
+  // en el then hacemos un redirect a /messages/iddelotro
+  const message = new Message({
+    from: req.user.id,
+    to: req.params.id,
+    text: req.body.text
+  })
+  message.save()
+  .then(() => res.redirect(`/messages/${req.params.id}`))
+  .catch(error => next(error))
+}
--- routes/message.routes.js
+++ routes/message.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const messageController = require ('../controllers/message.controller');
 
-router.get('messages/:userId', messageController.getRoom);
-// router.post('/messages/:userId', messageController.sendMessage);
+router.get('/:id', messageController.getRoom);
+router.post('/:id', messageController.sendMessage);
 
 module.exports = router;
